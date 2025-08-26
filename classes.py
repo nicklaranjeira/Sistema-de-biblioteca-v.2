8791c00393f98a229e80cd0dc5164a114dbbb06c
gabiungl <gabiungarette@gmail.com>
2025-08-26 13:11:04 -0300

    classes

--- classes.py
+++ classes.py
@@ -1,1 +1,45 @@
+class Emprestado:
+    def __init__(self, titulo, autor, genero, situacao):
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__genero = genero
+        self.__situacao= situacao
 
+class Disponiveis:
+    def __init__(self, titulo, autor, genero, situacao):
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__genero = genero
+        self.__situacao= situacao
+    # ----------------------------------------------------------------------
+# Metódos GETS and SETs 
+#GETs
+
+def getTitulo(self):
+    return self.__titulo
+
+def getAutor(self):
+    return self.__autor
+
+def getGenero(self):
+    return self.__genero
+
+def getSituacao(self):
+    return self.__situacao
+
+#SETs 
+def setTitulo(self,titulo):
+    self.__titulo = titulo
+    return self.__titulo
+
+def setAutor(self,autor):
+    self.__autor = autor
+    return self.__autor
+
+def setGenero(self,genero):
+    self.__genero = genero
+    return self.__genero
+
+def setSituacao(self,situacao):
+    self.__situacao = situacao
+    return self.__situacao
