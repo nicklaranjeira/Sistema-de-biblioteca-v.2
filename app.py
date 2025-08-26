f37183aba06ab500fd2319f02135edc67306f6d7
Gabriela Lopes <gabiungarette@gmail.com>
2025-08-26 18:38:05 -0300

    menu principal v1

--- app.py
+++ app.py
@@ -1,2 +1,29 @@
+import os
 from funcoes import *
-#listagem()
+
+while True:
+    print("Bem-vindo ao sistema de biblioteca!")
+    print("Selecione a opção que deseja:")
+    print("1 - Listar livros")
+    print("2 - Adicionar livro")
+    print("3 - Remover livro")
+    print("4 - Editar livro")
+    print("5 - Emprestar livro")
+    print("6 - Devolver livro")
+    print("0 - Sair")
+    opcao = int(input("Opção: "))
+
+    if opcao == 1:
+        menu_listar(livro)
+    elif opcao == 2:
+        adicionar_livro(livro)
+    elif opcao == 3:
+        remover_livro(livro, None)  
+    elif opcao == 4:
+        editar_livro(livro)
+    elif opcao == 0:
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida.")
+        continue
