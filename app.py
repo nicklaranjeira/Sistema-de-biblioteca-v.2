f6649c1384fcb90fd91684fe4918b3b3900f9b04
Gabriela Lopes <gabiungarette@gmail.com>
2025-09-01 18:11:24 -0300

    documneta~

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-import os #importa a biblioteca os 
-from funcoes import * #importa as funções 
-from classes import * #importa as classes 
-
-while True:
+while True: # loop principal do sistema
     print("Escolha:\n\n1-Bibliotecário\n2-Usuário\n0-Sair\n")
-    user = int(input("---->"))
-    if user == 1:
+    user = int(input("---->")) # entrada do usuario
+
+    if user == 1: # acesso como bibliotecario
         os.system('cls')
         menu_bibliotecario()
-    elif user == 2:
-            os.system('cls')
-            menu_user()
-    elif user == 0:
+    elif user == 2: # acesso como usuario
+        os.system('cls')
+        menu_user()
+    elif user == 0: # sair do sistema
          os.system('cls')
          print('Saindo...')
-         os.system('cls')
          break
-    else:
+    else: # opcao invalida
         os.system('cls')
         print("Opção inválida tente novamente!")
